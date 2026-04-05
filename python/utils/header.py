ec755020d510977408c8356391456234f742561f
Pablo Ospino <pospino.holnex@gmail.com>
2026-04-05 18:42:15 -0500

    feat: Enhance header_management to handle query parameters and raise KeyError for missing keys and data

--- python/utils/header.py
+++ python/utils/header.py
@@ -5,3 +5,35 @@ def params_parser(value):
         return json.loads(value)
     except (json.JSONDecodeError, TypeError):
         return value
+    
+
+def header_management(event: dict, m_keys=[], m_data=[], id_param_name=None):
+    params = {}
+    data   = {}
+    body   = event.get('body', {})
+    
+    if body:
+        data = json.loads(body)['data'] if isinstance(body, str) else body['data']
+    
+    # Parameters management
+    if 'pathParameters' in event:
+        p_params = event['pathParameters']
+        params.update({k: params_parser(v) for k, v in p_params.items()})
+
+    if 'queryStringParameters' in event:
+        q_params = event['queryStringParameters']
+        params.update({k: params_parser(v) for k, v in q_params.items()})
+
+    if id_param_name in params:
+        params['id'] = params.pop(id_param_name)
+
+    missing_keys = [ f'param.{k}' for k in m_keys if k not in params ]
+    missing_data = [ f'data.{k}' for k in m_data if k not in data ]
+
+    if missing_keys:
+        raise KeyError(missing_keys)
+
+    if missing_data:
+        raise KeyError(missing_data)
+        
+    return params, data
